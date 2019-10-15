193b50992bee3fb5f0949e00dfcd0153b75f1e4d
wanghaoyu33437 <578231980@qq.com>
2019-10-15 08:02:02 -0700

    thewholework

---
+++ week6/code/Readme.txt
@@ -0,0 +1,2 @@
+# staticlib is a folder which is run in local
+# exam is run in other envirment
---
+++ week6/code/exam/remotetest1.c
@@ -0,0 +1,11 @@
+#include "../staticlib/staticlib.h"
+#include <stdio.h>
+
+int main(){
+	int a[5]={-12,0,-454,7688,334};
+	showarray(a,5);
+	printf("max= %4d\n",max(a,5));
+	printf("sum= %4d\n",sum(a,5));
+	return 0;
+
+}
