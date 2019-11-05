0cb9bcba4f73c761335ba634073f2c3d3476d4d5
wanghaoyu33437 <578231980@qq.com>
2019-11-04 16:40:51 -0800

    进程及进程地址验证

---
+++ week8/code/process1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+	printf("pid -> %d\n",getpid());
+	printf("ppid -> %d\n",getppid() );
+	return 0;
+}
---
+++ week8/code/processEnv.c
@@ -0,0 +1,55 @@
+/*#include<stdio.h>
+
+int main(int argc,char *argv[],char *env[])
+
+{
+
+int i = 0;
+
+for(;env[i];i++)
+
+{
+
+printf("%s\n",env[i]);
+
+}
+
+return 0;
+
+}*/
+/*
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+
+{
+
+extern char **environ;
+
+int i = 0;
+
+for(; environ[i];i++)
+
+{
+
+printf("%s\n",environ[i]);
+
+}
+
+return 0;
+
+}*/
+
+#include<stdio.h>
+
+#include<stdlib.h>
+
+int main()
+
+{
+
+printf("%s\n",getenv("PATH"));
+
+return 0;
+
+}
