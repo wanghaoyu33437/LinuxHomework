9600c699b5ccde27b28b07b9fa8dbdabb3178774
wanghaoyu33437 <578231980@qq.com>
2019-12-16 17:45:52 -0800

    week15 - 1

---
+++ week14/code1/14-1.c
@@ -0,0 +1,27 @@
+#include "my.h"
+int arg=0;
+
+int foo(void *v){
+int n =(int )v;
+arg++;
+printf("%d :global arg=%d \n",n,arg);
+int arg;
+arg=n*100;
+printf("%d : local arg= %d \n",n,arg);
+}
+
+int main()
+{
+	pthread_t tid[NUM];
+	int rev[NUM];
+	for(int i =0;i<NUM;i++){
+		rev[i]=pthread_create(&tid[i],NULL,(void *(*)())foo,(void *)i);
+		if(rev[i]!=0){
+			perror("thread failed ! \n");
+			return -1;		
+		}
+	
+	printf("master %d : arg=%d\n",i,arg);
+	pthread_join(tid[i],NULL);
+	}
+}
---
+++ week14/code1/my.h
@@ -0,0 +1,9 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+int NUM=4;
